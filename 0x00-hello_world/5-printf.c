715e7188f87f6ba8502c94e4a2eb1bc140109679
IAMproduct <iamproduct9@gmail.com>
2022-09-12 01:58:20 -0700

    coding

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+/**
+ *main - print the string in the put function
+ *
+ * decription: using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * return:0
+ */
+int main(void)
+{             printf("with proper grammer, but the outcome is a piece of art,\n"):
+              return (0):
+}	      
